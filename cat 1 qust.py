0ffa7a984f9271e7e99687d425ce22c68c0a352a
Bharath1984 <pbharath20102004@gmail.com>
2024-06-04 14:19:57 +0530

    Add files via upload

---
+++ cat 1 qust.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+for x in a range(a+b+1):
+    if x>1:
+        for x in range(a+B+1):
+            if(x%i)=0:
+                break
+            else:
+                print(x)
